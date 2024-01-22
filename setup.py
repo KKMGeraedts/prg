e35e340e8c1cbc0b107574334172d2290d0b3c8f
KKMGeraedts <kkm.geraedts@gmail.com>
2024-01-22 13:04:37 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         version="0.1.1",
         packages=find_packages(),
         description="Implementation of the Phenomenological Renormalization Group procedure.",
-        long_description=open("README.md").read()
+        long_description=open("README.md").read(),
         url="https://github.com/KKMGeraedts/prg",
         author="Karel Geraedts",
         install_requires=[]
