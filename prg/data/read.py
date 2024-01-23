c8bb5c2860abb45dfc24bd47bce16ad743c9a82e
KKMGeraedts <kkm.geraedts@gmail.com>
2024-01-23 22:01:40 +0100

    Can now read txt files generated by custom Wolff algorithm

--- prg/data/read.py
+++ prg/data/read.py
@@ -4,7 +4,7 @@ import sys
 def read_input(input_file):
     """
     Reads filename from arguments else asks user for input if no filename was given.
-    So far only works with numpy specific file formats: .npy and .dat.
+    So far only works with numpy specific file formats: .txt,.npy and .dat.
 
     Return:
         Numpy array containing data.
@@ -14,7 +14,17 @@ def read_input(input_file):
         return np.load(f"./{input_file}")
     elif input_file[-3:] == "dat":
         # Assuming .dat files contain at each row a binary string, eg. '00..01100'
-        return np.genfromtxt(f"./{input_file}", delimiter=1, dtype=np.int8)  
+        return np.genfromtxt(f"./{input_file}", delimiter=1, dtype=np.int8)
+    elif input_file[-3] == "txt":
+        return read_wolffcpp_ising_data(input_file)
     else:
         print("Make sure input file has extensions .npy or .dat")
         exit()
+
+def read_wolffcpp_ising_data(input_file):
+    lattice_configurations = []
+    with open(input_file, "r") as file:
+        samples = []
+        for line in file:
+            samples.append([float(i) for i in line.split(" ")[:-1]])
+    return np.array(samples)
