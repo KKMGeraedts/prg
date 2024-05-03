25d8c54f5b1d7e3f8eeb174145279063382c1876
KKMGeraedts <kkm.geraedts@gmail.com>
2024-05-03 11:29:07 +0200

    changed version nr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name="prg",
-        version="0.1.1",
+        version="0.1.2",
         packages=find_packages(),
         description="Implementation of the Phenomenological Renormalization Group procedure.",
         long_description=open("README.md").read(),
